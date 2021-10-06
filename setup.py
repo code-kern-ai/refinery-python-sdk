9ac941f6fe2761f81b94ae2085d745a89a80ea1a
Johannes Hötter <johanneshotter@MacBook-Pro-von-Johannes.local>
2021-10-06 09:14:17 +0200

    improves auto_lf

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "README.md")) as file:
 
 setup(
     name="onetask",
-    version="0.1.10",
+    version="0.1.11",
     author="onetask",
     author_email="info@onetask.ai",
     description="Official Python SDK for the onetask API",
