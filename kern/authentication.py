2b28f48be33a50067eda743aa129f5153f8642d9
Johannes Hötter <jhoetter@MBPvonJohannes.fritz.box>
2022-05-07 14:33:47 +0200

    update to kern

---
+++ kern/authentication.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from kern import settings
+import requests
+
+
+def create_session_token(user_name: str, password: str) -> str:
+    headers = {"Accept": "application/json"}
+    action_url = (
+        requests.get(settings.get_authentication_url(), headers=headers)
+        .json()
+        .get("ui")
+        .get("action")
+    )
+    session_token = (
+        requests.post(
+            action_url,
+            headers=headers,
+            json={
+                "method": "password",
+                "password": password,
+                "password_identifier": user_name,
+            },
+        )
+        .json()
+        .get("session_token")
+    )
+    return session_token
