502e06da4d99e531b115c28af7a3907c6bd9769c
Johannes Hötter <johannes.hoetter@kern.ai>
2022-07-11 19:32:19 +0200

    adds metadata task

--- kern/adapter/rasa.py
+++ kern/adapter/rasa.py
@@ -26,14 +26,38 @@ def build_literal_from_iterable(iterable):
 
 
 def build_intent_yaml(
-    client, input_name, label_name, dir_name="data", file_name="nlu.yml"
+    client,
+    text_name,
+    intent_label_task,
+    metadata_label_task=None,
+    dir_name="data",
+    file_name="nlu.yml",
 ):
     df = client.get_record_export(tokenize=False)
 
     nlu_list = []
-    for label, df_label in df.groupby(label_name):
-        literal_string = build_literal_from_iterable(df_label[input_name].tolist())
-        nlu_list.append(OrderedDict(intent=label, examples=literal(literal_string)))
+    for label, df_sub_label in df.groupby(intent_label_task):
+
+        if metadata_label_task is not None:
+            metadata_label_name = metadata_label_task.split("__")[1]
+            for metadata_label, df_sub_label_sub_metadata_label in df_sub_label.groupby(
+                metadata_label_task
+            ):
+                literal_string = build_literal_from_iterable(
+                    df_sub_label_sub_metadata_label[text_name].tolist()
+                )
+                nlu_list.append(
+                    OrderedDict(
+                        intent=label,
+                        metadata=OrderedDict(**{metadata_label_name: metadata_label}),
+                        examples=literal(literal_string),
+                    )
+                )
+        else:
+            literal_string = build_literal_from_iterable(
+                df_sub_label[text_name].tolist()
+            )
+            nlu_list.append(OrderedDict(intent=label, examples=literal(literal_string)))
     nlu_dict = OrderedDict(nlu=nlu_list)
 
     if dir_name is not None and not os.path.isdir(dir_name):
